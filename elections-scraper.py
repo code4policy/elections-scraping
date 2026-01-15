15a48e897a65eb3a7df896edfc69b779d3fa6966
Dhrumil Mehta <dhrumil.mehta@columbia.edu>
2026-01-15 17:16:55 -0500

    add instructions to file

--- elections-scraper.py
+++ elections-scraper.py
@@ -1,1 +1,15 @@
-# Your Code Here
+# Copy/paste the code from the IMDB scraper ONE LINE AT A TIME
+#
+# When you paste the line, review what it does and modify it to 
+# scrape this elections website instead of IMDB
+
+import requests
+url = "https://electionstats.state.ma.us/elections/search/year_from:2024/year_to:2025"
+
+# get the page
+
+# extract HTML as structured "soup"
+
+# get each row of the table (what is the css selector for the whole row?)
+
+# Loop through the rows and extract the year
